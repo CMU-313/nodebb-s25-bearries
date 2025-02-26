40e2d1b8c5bd911f190edb8dba25fc1ae091caeb
Lynn Zhang <lynnzhan@andrew.cmu.edu>
2025-02-26 14:58:19 -0500

    react.yaml added, events.js yuki changes added, posts.js added reactions in, reactions.js in socket.io added

--- public/src/client/topic/events.js
+++ public/src/client/topic/events.js
@@ -17,6 +17,8 @@ define('forum/topic/events', [
 	const events = {
 		'event:user_status_change': onUserStatusChange,
 		'event:voted': updatePostVotesAndUserReputation,
+		//YUKI CHANGE
+		'event:reacted': updatePostReactions,
 		'event:bookmarked': updateBookmarkCount,
 
 		'event:topic_deleted': threadTools.setDeleteState,
@@ -44,6 +46,10 @@ define('forum/topic/events', [
 		'posts.downvote': togglePostVote,
 		'posts.unvote': togglePostVote,
 
+		// YUKICHANGE
+		'posts.react': togglePostReact,
+		'posts.unreact': togglePostReact,
+
 		'event:new_notification': onNewNotification,
 		'event:new_post': posts.onNewPost,
 	};
@@ -64,6 +70,24 @@ define('forum/topic/events', [
 			}
 		}
 	};
+
+	// YUKICHANGE: added & removed reputation related updates
+	function updatePostReactions(data) {
+		const reactions = $('[data-pid="' + data.post.pid + '"] [component="post/reaction-count"]').filter(function (index, el) {
+			return parseInt($(el).closest('[data-pid]').attr('data-pid'), 10) === parseInt(data.post.pid, 10);
+			// reactions.html(data.post.reactions).attr('data-votes', data.post.reactions);
+		});
+	}
+	// YUKICHANGE
+	function togglePostReact(data) {
+		const post = $('[data-pid="' + data.post.pid + '"]');
+		post.find('[component="post/react"]').filter(function (index, el) {
+			return parseInt($(el).closest('[data-pid]').attr('data-pid'), 10) === parseInt(data.post.pid, 10);
+		}).toggleClass('reacted', data.react);
+		// post.find('[component="post/unreact"]').filter(function (index, el) {
+		// 	return parseInt($(el).closest('[data-pid]').attr('data-pid'), 10) === parseInt(data.post.pid, 10);
+		// }).toggleClass('downvoted', data.downvote);
+	}
 
 	function onUserStatusChange(data) {
 		app.updateUserStatus($('[data-uid="' + data.uid + '"] [component="user/status"]'), data.status);
